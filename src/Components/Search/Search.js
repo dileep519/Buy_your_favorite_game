ea875454858ec830f76857c5fc9e76c44d137b92
= <=>
2020-07-15 23:07:00 +0530

    second commit

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -76,8 +76,4 @@ export default Search;
 
 
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git remote add origin https://github.com/dileep519/Buy_your_favorite_game.git
-// git push -u origin master
+
